d907811a2d09e7d33baecb3cdd43082090f05457
Bob Torgerson <rltorgerson@alaska.edu>
2015-09-21 15:13:19 -0800

    Consolidated script down into loops to automate capture and projection of differing datasets based on one loop for variable and one for date.

--- geonode/NCEP/ncep_convert.sh
+++ geonode/NCEP/ncep_convert.sh
@@ -5,67 +5,61 @@
 # a GeoTIFF from the NetCDF file. These raster files are currently being output in EPSG:4326, which matches the GeoNode's
 # default projection for its OpenLayers maps.
 
-#################### [ DAILY ] #################### 
-# Set the day, month, and year we are requesting. Most recent data on NCEP website is from 2 days ago.
-day=`date --date='-2 days' +%d`
-month=`date --date='-2 days' +%m`
-year=`date --date='-2 days' +%Y`
+nc_variables=('Air+Temperature'
+	      'Sea+Surface+Temperature')
 
-# Capture the HTML from the GET request for our data. Place it into a text file.
-curl "http://www.esrl.noaa.gov/psd/cgi-bin/data/composites/comp.day.pl?var=Air%20Temperature;level=Surface;iy%5B1%5D=;im%5B1%5D=;id%5B1%5D=;iy%5B2%5D=;im%5B2%5D=;id%5B2%5D=;iy%5B3%5D=;im%5B3%5D=;id%5B3%5D=;iy%5B4%5D=;im%5B4%5D=;id%5B4%5D=;iy%5B5%5D=;im%5B5%5D=;id%5B5%5D=;iy%5B6%5D=;im%5B6%5D=;id%5B6%5D=;iy%5B7%5D=;im%5B7%5D=;id%5B7%5D=;iy%5B8%5D=;im%5B8%5D=;id%5B8%5D=;iy%5B9%5D=;im%5B9%5D=;id%5B9%5D=;iy%5B10%5D=;im%5B10%5D=;id%5B10%5D=;iy%5B11%5D=;im%5B11%5D=;id%5B11%5D=;iy%5B12%5D=;im%5B12%5D=;id%5B12%5D=;iy%5B13%5D=;im%5B13%5D=;id%5B13%5D=;iy%5B14%5D=;im%5B14%5D=;id%5B14%5D=;iy%5B15%5D=;im%5B15%5D=;id%5B15%5D=;iy%5B16%5D=;im%5B16%5D=;id%5B16%5D=;iy%5B17%5D=;im%5B17%5D=;id%5B17%5D=;iy%5B18%5D=;im%5B18%5D=;id%5B18%5D=;iy%5B19%5D=;im%5B19%5D=;id%5B19%5D=;iy%5B20%5D=;im%5B20%5D=;id%5B20%5D=;monr1=$month;dayr1=$day;monr2=$month;dayr2=$day;iyr%5B1%5D=$year;filenamein=;plotlabel=;lag=-1;labelc=Color;labels=Shaded;type=1;scale=;label=0;cint=;lowr=;highr=;istate=0;proj=ALL;xlat1=0;xlat2=90;xlon1=0;xlon2=360;custproj=Northern%20Hemisphere%20Polar%20Stereographic;level1=1000mb;level2=10mb;Submit=Create%20Plot" > daily.txt
+file_names=('air_temp'
+	    'sea_surf_temp')
 
-# Some BASH "magic" to get just the full path to the created NetCDF file.
-daily=`grep "\.nc" daily.txt | awk '{ print $2; }' | cut -c6- | rev | cut -c5- | rev`
+var_names=('air'
+           'skt')
 
-# Download the NetCDF file.
-wget "http://www.esrl.noaa.gov/$daily"
+dates=('-2 days'
+       '-7 days'
+       '-1 month'
+       '-1 year')
 
-# Rename the file to daily.nc rather than its randomly generated name.
-mv ${daily##*/} daily.nc
+total_vars=${#nc_variables[*]}
 
-# GDAL translate the NetCDF file into a GeoTIFF with EPSG:4326 projection called daily.tif.
-gdal_translate -a_srs EPSG:4326 -of GTiff netCDF:"daily.nc":air daily.tif
+for (( i=0; i< $(($total_vars)); i++)); do
+  for cur_date in "${dates[@]}"; do
+    day=`date --date="$cur_date" +%d`
+    month=`date --date="$cur_date" +%m`
+    year=`date --date="$cur_date" +%Y`
 
-# All of the rest of the code is currently repeats of the above code for different days. The comments
-# above should be similarly relavent to the code below.
+    # Capture the HTML from the GET request for our data. Place it into a text file.
+    curl "http://www.esrl.noaa.gov/psd/cgi-bin/data/composites/comp.day.pl?var=Sea+Surface+Temperature&level=Surface&iy%5B1%5D=&im%5B1%5D=&id%5B1%5D=&iy%5B2%5D=&im%5B2%5D=&id%5B2%5D=&iy%5B3%5D=&im%5B3%5D=&id%5B3%5D=&iy%5B4%5D=&im%5B4%5D=&id%5B4%5D=&iy%5B5%5D=&im%5B5%5D=&id%5B5%5D=&iy%5B6%5D=&im%5B6%5D=&id%5B6%5D=&iy%5B7%5D=&im%5B7%5D=&id%5B7%5D=&iy%5B8%5D=&im%5B8%5D=&id%5B8%5D=&iy%5B9%5D=&im%5B9%5D=&id%5B9%5D=&iy%5B10%5D=&im%5B10%5D=&id%5B10%5D=&iy%5B11%5D=&im%5B11%5D=&id%5B11%5D=&iy%5B12%5D=&im%5B12%5D=&id%5B12%5D=&iy%5B13%5D=&im%5B13%5D=&id%5B13%5D=&iy%5B14%5D=&im%5B14%5D=&id%5B14%5D=&iy%5B15%5D=&im%5B15%5D=&id%5B15%5D=&iy%5B16%5D=&im%5B16%5D=&id%5B16%5D=&iy%5B17%5D=&im%5B17%5D=&id%5B17%5D=&iy%5B18%5D=&im%5B18%5D=&id%5B18%5D=&iy%5B19%5D=&im%5B19%5D=&id%5B19%5D=&iy%5B20%5D=&im%5B20%5D=&id%5B20%5D=&monr1=$month&dayr1=$day&monr2=$month&dayr2=$day&iyr%5B1%5D=$year&filenamein=&plotlabel=&lag=0&labelc=Color&labels=Shaded&type=1&scale=&label=0&cint=&lowr=&highr=&istate=0&proj=ALL&xlat1=&xlat2=&xlon1=&xlon2=&custproj=Northern+Hemisphere+Polar+Stereographic&level1=1000mb&level2=10mb&Submit=Create+Plot" > temporary.txt
 
+    # Some BASH "magic" to get just the full path to the created NetCDF file.
+    temporary=`grep "\.nc" temporary.txt | awk '{ print $2; }' | cut -c6- | rev | cut -c5- | rev`
+    rm -f temporary.txt
 
-#################### [ WEEKLY ] #################### 
-day=`date --date='-7 days' +%d`
-month=`date --date='-7 days' +%m`
-year=`date --date='-7 days' +%Y`
+    # Download the NetCDF file.
+    wget "http://www.esrl.noaa.gov/$temporary"
 
-curl "http://www.esrl.noaa.gov/psd/cgi-bin/data/composites/comp.day.pl?var=Air%20Temperature;level=Surface;iy%5B1%5D=;im%5B1%5D=;id%5B1%5D=;iy%5B2%5D=;im%5B2%5D=;id%5B2%5D=;iy%5B3%5D=;im%5B3%5D=;id%5B3%5D=;iy%5B4%5D=;im%5B4%5D=;id%5B4%5D=;iy%5B5%5D=;im%5B5%5D=;id%5B5%5D=;iy%5B6%5D=;im%5B6%5D=;id%5B6%5D=;iy%5B7%5D=;im%5B7%5D=;id%5B7%5D=;iy%5B8%5D=;im%5B8%5D=;id%5B8%5D=;iy%5B9%5D=;im%5B9%5D=;id%5B9%5D=;iy%5B10%5D=;im%5B10%5D=;id%5B10%5D=;iy%5B11%5D=;im%5B11%5D=;id%5B11%5D=;iy%5B12%5D=;im%5B12%5D=;id%5B12%5D=;iy%5B13%5D=;im%5B13%5D=;id%5B13%5D=;iy%5B14%5D=;im%5B14%5D=;id%5B14%5D=;iy%5B15%5D=;im%5B15%5D=;id%5B15%5D=;iy%5B16%5D=;im%5B16%5D=;id%5B16%5D=;iy%5B17%5D=;im%5B17%5D=;id%5B17%5D=;iy%5B18%5D=;im%5B18%5D=;id%5B18%5D=;iy%5B19%5D=;im%5B19%5D=;id%5B19%5D=;iy%5B20%5D=;im%5B20%5D=;id%5B20%5D=;monr1=$month;dayr1=$day;monr2=$month;dayr2=$day;iyr%5B1%5D=$year;filenamein=;plotlabel=;lag=-1;labelc=Color;labels=Shaded;type=1;scale=;label=0;cint=;lowr=;highr=;istate=0;proj=ALL;xlat1=0;xlat2=90;xlon1=0;xlon2=360;custproj=Northern%20Hemisphere%20Polar%20Stereographic;level1=1000mb;level2=10mb;Submit=Create%20Plot" > weekly.txt 
+    # Rename the file to daily.nc rather than its randomly generated name.
+    mv ${temporary##*/} temporary.nc
 
-weekly=`grep "\.nc" weekly.txt | awk '{ print $2; }' | cut -c6- | rev | cut -c5- | rev`
-wget "http://www.esrl.noaa.gov/$weekly"
-mv ${weekly##*/} weekly.nc
-gdal_translate -a_srs EPSG:4326 -of GTiff netCDF:"weekly.nc":air weekly.tif
+    case $cur_date in
+      '-2 days')
+        tif_name="daily_${file_names[$i]}.tif"
+        ;;
+      '-7 days')
+        tif_name="weekly_${file_names[$i]}.tif"
+        ;;
+      '-1 month')
+        tif_name="monthly_${file_names[$i]}.tif"
+        ;;
+      '-1 year')
+        tif_name="yearly_${file_names[$i]}.tif"
+        ;;
+    esac
 
-
-#################### [ MONTHLY ] #################### 
-day=`date --date='-1 month' +%d`
-month=`date --date='-1 month' +%m`
-year=`date --date='-1 month' +%Y`
-
-curl "http://www.esrl.noaa.gov/psd/cgi-bin/data/composites/comp.day.pl?var=Air%20Temperature;level=Surface;iy%5B1%5D=;im%5B1%5D=;id%5B1%5D=;iy%5B2%5D=;im%5B2%5D=;id%5B2%5D=;iy%5B3%5D=;im%5B3%5D=;id%5B3%5D=;iy%5B4%5D=;im%5B4%5D=;id%5B4%5D=;iy%5B5%5D=;im%5B5%5D=;id%5B5%5D=;iy%5B6%5D=;im%5B6%5D=;id%5B6%5D=;iy%5B7%5D=;im%5B7%5D=;id%5B7%5D=;iy%5B8%5D=;im%5B8%5D=;id%5B8%5D=;iy%5B9%5D=;im%5B9%5D=;id%5B9%5D=;iy%5B10%5D=;im%5B10%5D=;id%5B10%5D=;iy%5B11%5D=;im%5B11%5D=;id%5B11%5D=;iy%5B12%5D=;im%5B12%5D=;id%5B12%5D=;iy%5B13%5D=;im%5B13%5D=;id%5B13%5D=;iy%5B14%5D=;im%5B14%5D=;id%5B14%5D=;iy%5B15%5D=;im%5B15%5D=;id%5B15%5D=;iy%5B16%5D=;im%5B16%5D=;id%5B16%5D=;iy%5B17%5D=;im%5B17%5D=;id%5B17%5D=;iy%5B18%5D=;im%5B18%5D=;id%5B18%5D=;iy%5B19%5D=;im%5B19%5D=;id%5B19%5D=;iy%5B20%5D=;im%5B20%5D=;id%5B20%5D=;monr1=$month;dayr1=$day;monr2=$month;dayr2=$day;iyr%5B1%5D=$year;filenamein=;plotlabel=;lag=-1;labelc=Color;labels=Shaded;type=1;scale=;label=0;cint=;lowr=;highr=;istate=0;proj=ALL;xlat1=0;xlat2=90;xlon1=0;xlon2=360;custproj=Northern%20Hemisphere%20Polar%20Stereographic;level1=1000mb;level2=10mb;Submit=Create%20Plot" > monthly.txt
-
-monthly=`grep "\.nc" monthly.txt | awk '{ print $2; }' | cut -c6- | rev | cut -c5- | rev`
-wget "http://www.esrl.noaa.gov/$monthly"
-mv ${monthly##*/} monthly.nc
-gdal_translate -a_srs EPSG:4326 -of GTiff netCDF:"monthly.nc":air monthly.tif
-
-
-#################### [ YEARLY ] #################### 
-day=`date --date='-1 year' +%d`
-month=`date --date='-1 year' +%m`
-year=`date --date='-1 year' +%Y`
-
-curl "http://www.esrl.noaa.gov/psd/cgi-bin/data/composites/comp.day.pl?var=Air%20Temperature;level=Surface;iy%5B1%5D=;im%5B1%5D=;id%5B1%5D=;iy%5B2%5D=;im%5B2%5D=;id%5B2%5D=;iy%5B3%5D=;im%5B3%5D=;id%5B3%5D=;iy%5B4%5D=;im%5B4%5D=;id%5B4%5D=;iy%5B5%5D=;im%5B5%5D=;id%5B5%5D=;iy%5B6%5D=;im%5B6%5D=;id%5B6%5D=;iy%5B7%5D=;im%5B7%5D=;id%5B7%5D=;iy%5B8%5D=;im%5B8%5D=;id%5B8%5D=;iy%5B9%5D=;im%5B9%5D=;id%5B9%5D=;iy%5B10%5D=;im%5B10%5D=;id%5B10%5D=;iy%5B11%5D=;im%5B11%5D=;id%5B11%5D=;iy%5B12%5D=;im%5B12%5D=;id%5B12%5D=;iy%5B13%5D=;im%5B13%5D=;id%5B13%5D=;iy%5B14%5D=;im%5B14%5D=;id%5B14%5D=;iy%5B15%5D=;im%5B15%5D=;id%5B15%5D=;iy%5B16%5D=;im%5B16%5D=;id%5B16%5D=;iy%5B17%5D=;im%5B17%5D=;id%5B17%5D=;iy%5B18%5D=;im%5B18%5D=;id%5B18%5D=;iy%5B19%5D=;im%5B19%5D=;id%5B19%5D=;iy%5B20%5D=;im%5B20%5D=;id%5B20%5D=;monr1=$month;dayr1=$day;monr2=$month;dayr2=$day;iyr%5B1%5D=$year;filenamein=;plotlabel=;lag=-1;labelc=Color;labels=Shaded;type=1;scale=;label=0;cint=;lowr=;highr=;istate=0;proj=ALL;xlat1=0;xlat2=90;xlon1=0;xlon2=360;custproj=Northern%20Hemisphere%20Polar%20Stereographic;level1=1000mb;level2=10mb;Submit=Create%20Plot" > yearly.txt
-
-yearly=`grep "\.nc" yearly.txt | awk '{ print $2; }' | cut -c6- | rev | cut -c5- | rev`
-wget "http://www.esrl.noaa.gov/$yearly"
-mv ${yearly##*/} yearly.nc
-gdal_translate -a_srs EPSG:4326 -of GTiff netCDF:"yearly.nc":air yearly.tif
-
+    # GDAL translate the NetCDF file into a GeoTIFF with EPSG:4326 projection called daily_air_temp.tif.
+    gdal_translate -a_srs EPSG:4326 -of GTiff netCDF:"temporary.nc":${var_name[$i]} temporary.tif
+    gdalwarp -t_srs WGS84 temporary.tif $tif_name -wo SOURCE_EXTRA=1000 --config CENTER_LONG 0
+    rm -f temporary.nc
+    rm -f temporary.tif
+  done
+done
 
