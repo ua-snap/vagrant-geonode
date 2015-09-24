d168131fb40896c48234e8d8930454c784a0e97f
Bob Torgerson <rltorgerson@alaska.edu>
2015-09-24 09:44:40 -0800

    Additional logic added to prevent faulty files from being downloaded due to dates being unavailable for download. Also, colors the temperature using a provided text file with colors represented in degrees Kelvin.

--- geonode/NCEP/ncep_convert.sh
+++ geonode/NCEP/ncep_convert.sh
@@ -5,6 +5,12 @@
 # a GeoTIFF from the NetCDF file. These raster files are currently being output in EPSG:4326, which matches the GeoNode's
 # default projection for its OpenLayers maps.
 
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 nc_variables=('Air+Temperature'
 	      'Sea+Surface+Temperature')
 
@@ -19,6 +25,10 @@ dates=('-2 days'
        '-1 month'
        '-1 year')
 
+printf "\n${RED}########################################################################\n"
+printf "#######################${YELLOW}[NCEP Historical Data]${RED}###########################\n"
+printf "########################################################################${NC}\n"
+
 total_vars=${#nc_variables[*]}
 
 for (( i=0; i< $(($total_vars)); i++)); do
@@ -27,15 +37,22 @@ for (( i=0; i< $(($total_vars)); i++)); do
     month=`date --date="$cur_date" +%m`
     year=`date --date="$cur_date" +%Y`
 
+    printf "\n${RED}Downloading ${YELLOW}${nc_variables[$i]} ${RED}for the date ${BLUE}$month/$day/$year ${RED}...\n" 
+    printf "This can take a few seconds...${NC}\n\n"
+
     # Capture the HTML from the GET request for our data. Place it into a text file.
-    curl "http://www.esrl.noaa.gov/psd/cgi-bin/data/composites/comp.day.pl?var=Sea+Surface+Temperature&level=Surface&iy%5B1%5D=&im%5B1%5D=&id%5B1%5D=&iy%5B2%5D=&im%5B2%5D=&id%5B2%5D=&iy%5B3%5D=&im%5B3%5D=&id%5B3%5D=&iy%5B4%5D=&im%5B4%5D=&id%5B4%5D=&iy%5B5%5D=&im%5B5%5D=&id%5B5%5D=&iy%5B6%5D=&im%5B6%5D=&id%5B6%5D=&iy%5B7%5D=&im%5B7%5D=&id%5B7%5D=&iy%5B8%5D=&im%5B8%5D=&id%5B8%5D=&iy%5B9%5D=&im%5B9%5D=&id%5B9%5D=&iy%5B10%5D=&im%5B10%5D=&id%5B10%5D=&iy%5B11%5D=&im%5B11%5D=&id%5B11%5D=&iy%5B12%5D=&im%5B12%5D=&id%5B12%5D=&iy%5B13%5D=&im%5B13%5D=&id%5B13%5D=&iy%5B14%5D=&im%5B14%5D=&id%5B14%5D=&iy%5B15%5D=&im%5B15%5D=&id%5B15%5D=&iy%5B16%5D=&im%5B16%5D=&id%5B16%5D=&iy%5B17%5D=&im%5B17%5D=&id%5B17%5D=&iy%5B18%5D=&im%5B18%5D=&id%5B18%5D=&iy%5B19%5D=&im%5B19%5D=&id%5B19%5D=&iy%5B20%5D=&im%5B20%5D=&id%5B20%5D=&monr1=$month&dayr1=$day&monr2=$month&dayr2=$day&iyr%5B1%5D=$year&filenamein=&plotlabel=&lag=0&labelc=Color&labels=Shaded&type=1&scale=&label=0&cint=&lowr=&highr=&istate=0&proj=ALL&xlat1=&xlat2=&xlon1=&xlon2=&custproj=Northern+Hemisphere+Polar+Stereographic&level1=1000mb&level2=10mb&Submit=Create+Plot" > temporary.txt
+    curl "http://www.esrl.noaa.gov/psd/cgi-bin/data/composites/comp.day.pl?var=${nc_variables[$i]}&level=Surface&iy%5B1%5D=&im%5B1%5D=&id%5B1%5D=&iy%5B2%5D=&im%5B2%5D=&id%5B2%5D=&iy%5B3%5D=&im%5B3%5D=&id%5B3%5D=&iy%5B4%5D=&im%5B4%5D=&id%5B4%5D=&iy%5B5%5D=&im%5B5%5D=&id%5B5%5D=&iy%5B6%5D=&im%5B6%5D=&id%5B6%5D=&iy%5B7%5D=&im%5B7%5D=&id%5B7%5D=&iy%5B8%5D=&im%5B8%5D=&id%5B8%5D=&iy%5B9%5D=&im%5B9%5D=&id%5B9%5D=&iy%5B10%5D=&im%5B10%5D=&id%5B10%5D=&iy%5B11%5D=&im%5B11%5D=&id%5B11%5D=&iy%5B12%5D=&im%5B12%5D=&id%5B12%5D=&iy%5B13%5D=&im%5B13%5D=&id%5B13%5D=&iy%5B14%5D=&im%5B14%5D=&id%5B14%5D=&iy%5B15%5D=&im%5B15%5D=&id%5B15%5D=&iy%5B16%5D=&im%5B16%5D=&id%5B16%5D=&iy%5B17%5D=&im%5B17%5D=&id%5B17%5D=&iy%5B18%5D=&im%5B18%5D=&id%5B18%5D=&iy%5B19%5D=&im%5B19%5D=&id%5B19%5D=&iy%5B20%5D=&im%5B20%5D=&id%5B20%5D=&monr1=$month&dayr1=$day&monr2=$month&dayr2=$day&iyr%5B1%5D=$year&filenamein=&plotlabel=&lag=0&labelc=Color&labels=Shaded&type=1&scale=&label=0&cint=&lowr=&highr=&istate=0&proj=ALL&xlat1=&xlat2=&xlon1=&xlon2=&custproj=Northern+Hemisphere+Polar+Stereographic&level1=1000mb&level2=10mb&Submit=Create+Plot" > temporary.txt 2>/dev/null
 
     # Some BASH "magic" to get just the full path to the created NetCDF file.
     temporary=`grep "\.nc" temporary.txt | awk '{ print $2; }' | cut -c6- | rev | cut -c5- | rev`
-    rm -f temporary.txt
+
+    if [ "$temporary" == "" ]; then
+        last_date=`grep "Last day is" temporary.txt | cut -c13- | rev | cut -c6- | rev`
+	printf "${RED}The last day available for the ${YELLOW}${nc_variables[$i]} ${RED}variable is ${BLUE}$last_date ${RED}. The GeoTIFF has not been updated.${NC}\n"
+    else
 
     # Download the NetCDF file.
-    wget "http://www.esrl.noaa.gov/$temporary"
+    wget "http://www.esrl.noaa.gov/$temporary" > /dev/null 2>&1
 
     # Rename the file to daily.nc rather than its randomly generated name.
     mv ${temporary##*/} temporary.nc
@@ -56,10 +73,16 @@ for (( i=0; i< $(($total_vars)); i++)); do
     esac
 
     # GDAL translate the NetCDF file into a GeoTIFF with EPSG:4326 projection called daily_air_temp.tif.
-    gdal_translate -a_srs EPSG:4326 -of GTiff netCDF:"temporary.nc":${var_name[$i]} temporary.tif
-    gdalwarp -t_srs WGS84 temporary.tif $tif_name -wo SOURCE_EXTRA=1000 --config CENTER_LONG 0
+    printf "${RED}Translating the NetCDF file into a GeoTiff file...${NC}\n"
+    gdal_translate -a_srs EPSG:4326 -of GTiff netCDF:"temporary.nc":${var_name[$i]} temporary.tif > /dev/null 2>&1
+    gdalwarp -t_srs WGS84 temporary.tif grey.tif -wo SOURCE_EXTRA=1000 --config CENTER_LONG 0 > /dev/null 2>&1
+    gdaldem color-relief grey.tif color.txt $tif_name > /dev/null 2>&1
+    printf "\n${GREEN}Successfully created ${RED}$tif_name ${NC}\n"
+    rm -f temporary.txt
     rm -f temporary.nc
     rm -f temporary.tif
+    rm -f grey.tif
+   fi
   done
 done
 
